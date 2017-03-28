f3eb4a9e2e197e52005252a87033dc137b28de68
Harsimar <simarsingh24@gmail.com>
2017-03-28 23:42:16 +0530

    facebook linked

--- app/src/main/java/com/svnit/harsimar/placementfeed/MainActivity.java
+++ app/src/main/java/com/svnit/harsimar/placementfeed/MainActivity.java
@@ -1,7 +1,18 @@
 package com.svnit.harsimar.placementfeed;
 
+import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
+
+import com.facebook.FacebookSdk;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +20,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        /*
+
+        Generating key hash
+        
+        try {
+
+            PackageInfo info = getPackageManager().getPackageInfo(
+                    "com.svnit.harsimar.placementfeed",
+                    PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.d("harsimarSigh", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+
+        } catch (NoSuchAlgorithmException e) {
+
+        }*/
+
     }
 }
