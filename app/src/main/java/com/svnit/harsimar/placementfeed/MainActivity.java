fb59e9f687ac97ba1a51ae0796d3d9f00d68c746
Harsimar <simarsingh24@gmail.com>
2017-03-29 15:21:16 +0530

    facebook login added

--- app/src/main/java/com/svnit/harsimar/placementfeed/MainActivity.java
+++ app/src/main/java/com/svnit/harsimar/placementfeed/MainActivity.java
@@ -1,29 +1,67 @@
 package com.svnit.harsimar.placementfeed;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.Signature;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telecom.Call;
 import android.util.Base64;
 import android.util.Log;
+import android.widget.Toast;
 
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
 public class MainActivity extends AppCompatActivity {
 
+    private LoginButton loginButton;
+    private CallbackManager callbackManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        callbackManager=CallbackManager.Factory.create();
+        loginButton =(LoginButton)findViewById(R.id.facebook_login_button);
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                Intent intent= new Intent(MainActivity.this,LoggedIn.class);
+                startActivity(intent);
+                Log.d("FacebookInfo",loginResult.getAccessToken().getUserId().toString().trim());
+                Log.d("FacebookInfo",loginResult.getAccessToken().getToken().toString().trim());
+
+            }
+
+            @Override
+            public void onCancel() {
+                Toast.makeText(MainActivity.this,"Login Cancelled",Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+
+            }
+        });
+
+
+
+
         /*
 
         Generating key hash
-        
+
         try {
 
             PackageInfo info = getPackageManager().getPackageInfo(
@@ -39,6 +77,12 @@ public class MainActivity extends AppCompatActivity {
         } catch (NoSuchAlgorithmException e) {
 
         }*/
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        callbackManager.onActivityResult(requestCode,resultCode,data);
 
     }
 }
